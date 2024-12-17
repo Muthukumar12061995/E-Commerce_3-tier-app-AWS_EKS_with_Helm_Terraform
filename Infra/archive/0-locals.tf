91703cd866ae555fb993a3fcc23d3c0e4e100b87
muthu <kumarmuthu.m12@gmail.com>
2024-12-17 12:59:36 +0400

    remove archive

--- Infra/archive/0-locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  region       = "us-east-1"
-  zone1        = "us-east-1a"
-  zone2        = "us-east-1b"
-  cluster_name = "dev-E-commerce"
-  eks_version  = "1.30"
-}
-
--- Infra/archive/8-security.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "ec2_ssh_key" {
-  public_key = file("~/.ssh/ec2.pub")
-  key_name   = "ec2-ssh"
-}
